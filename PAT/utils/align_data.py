ac1de0e7e835d631ad8a9006e16d2a5e47d50e59
Sam Motamed <61124645+sam-motamed@users.noreply.github.com>
2023-09-07 10:15:56 +0300

    Add files via upload

---
+++ PAT/utils/align_data.py
@@ -0,0 +1,37 @@
+from configs import paths_config
+import dlib
+import glob
+import os
+from tqdm import tqdm
+from utils.alignment import align_face
+
+
+def pre_process_images(raw_images_path):
+    current_directory = os.getcwd()
+
+    IMAGE_SIZE = 512
+    predictor = dlib.shape_predictor(paths_config.dlib)
+    os.chdir(raw_images_path)
+    images_names = glob.glob(f'*.jpg')
+    print(images_names)
+    aligned_images = []
+    for image_name in tqdm(images_names):
+        try:
+            aligned_image = align_face(filepath=f'./{image_name}',
+                                       predictor=predictor, output_size=IMAGE_SIZE)
+            aligned_images.append(aligned_image)
+        except Exception as e:
+            print(e)
+
+    #os.makedirs(paths_config.input_data_path, exist_ok=True)
+    for image, name in zip(aligned_images, images_names):
+        real_name = name.split('.')[0]
+        print(real_name)
+        print(os.getcwd())
+        image.save(f'../NAME-TO-BE-SAVED-AS/{real_name}.jpg')
+
+    os.chdir(current_directory)
+
+
+if __name__ == "__main__":
+    pre_process_images('./FILE-PATH-TO-BE-ALIGNED')
